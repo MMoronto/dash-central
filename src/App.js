09fd28be426156055d4d825cac368896b5de8b4c
MMORONTO <t2wycemo@gmail.com>
2022-06-03 00:02:18 -0500

    create FiSettings button element

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ const App = () => {
             <div className="flex relative dark:bg-main-dark-bg">
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
                     <TooltipComponent content="Settings" position="Top">
-
+                        <button>
+                            <FiSettings />
+                        </button>
                     </TooltipComponent>
                 </div>
             </div>
